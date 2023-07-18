91422dee504a7075f4826f8cb3c2bccfc05fca52
ish <ish@ish.works>
2023-07-18 22:39:11 +0300

    added some sample code

--- example.ts
+++ example.ts
@@ -1,19 +1,4 @@
-furry-text-search
------------------
-
-furry-text-search is a library written in TypeScript to handle furry/fuzzy, imprecise, forgiving text search. It is based on [Fuse.js](https://github.com/krisk/Fuse) albeit greatly simplified, and converted to proper TypeScript.
-
-This library is probably not for everyone. I forked it specifically to use [in a project](https://bus-alerts.com/) where I needed a library like Fuse.js, but didn't need all of its features.
-
-If you need more advanced features, there's a good chance you shouldn't be using this library lol
-
-How to use
-==========
-
-(The same code sample is also available in [example.ts](example.ts))
-
-```typescript
-import { FurryIndex } from "furry-text-search";
+import { FurryIndex } from "./dist";
 
 type BusLine = {
     routeShortName: string;
@@ -160,4 +145,3 @@ console.log(result);
     ]
   }
 ]
-```
